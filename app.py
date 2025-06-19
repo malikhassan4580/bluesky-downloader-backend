fee7b1c3ae35c4a30fae770c358d619df422084f
malikhassan4580 <37020172+malikhassan4580@users.noreply.github.com>
2025-06-19 16:27:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, send_file, jsonify
+from flask_cors import CORS
 import requests
 import re
 from io import BytesIO
-
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/download', methods=['POST'])
 def download():
